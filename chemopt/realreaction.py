30bb3d723bfea4922d121ed9e9616fc352f30c62
jangerit <jan.rittig@rwth-aachen.de>
2020-05-29 13:45:52 +0200

    update rnn dep 2

--- chemopt/realreaction.py
+++ chemopt/realreaction.py
@@ -4,7 +4,7 @@ import logging
 import matplotlib.pyplot as plt
 import json
 
-import .rnn
+from rnn import *
 from reactions import QuadraticEval, ConstraintQuadraticEval, RealReaction
 from logger import get_handlers
 from collections import namedtuple
